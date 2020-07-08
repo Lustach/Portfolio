772abe191a29cdb2a0782924dc095c01c25ffeeb
lusta.vm <lusta.vlad2001@gmail.com>
2020-07-08 09:11:32 +0300

    ADD: slider

--- js/slider.js
+++ js/slider.js
@@ -2,27 +2,29 @@ document.addEventListener('DOMContentLoaded', function () {
 	let slideIndex = 1
 	let slides = document.querySelectorAll(".modal-work")
 	showSlides(slideIndex)
-	let prev = document.querySelector('.prev')
-	let next = document.querySelector('.next')
+	let prev = document.querySelectorAll('.prev')
+	let next = document.querySelectorAll('.next')
 	console.log(prev, next)
-	prev.onclick = () => {
+	prev.forEach(el => el.addEventListener('click', e => {
 		showSlides(slideIndex -= 1)
-	}
-	next.onclick = () => {
+	}))
+	next.forEach(el => el.addEventListener('click', e => {
 		showSlides(slideIndex += 1)
-	}
+	}))
+	next.addEventListener('click', () => {
+		showSlides(slideIndex += 1)
+	})
 	/* Основная функция слайдера */
 	function showSlides(n) {
-		// todo доделать слайдер
-		if (n < 1) {
-			slides.forEach((e, i, arr) => {
-				console.log(arr.length)
-				if (i < arr.length-1)
-					e.style.display = 'none'
-				else{
-					e.style.display = 'flex'
-				}
-			})
+		if (n > slides.length) {
+			slideIndex = 1
 		}
+		if (n < 1) {
+			slideIndex = slides.length
+		}
+		for (let i = 0; i < slides.length; i++) {
+			slides[i].style.display = "none"
+		}
+		slides[slideIndex - 1].style.display = "flex"
 	}
 }, false)
